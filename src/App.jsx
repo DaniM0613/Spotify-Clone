ccc0a26c0bdb99d43776bef66135c1af829f0a70
DaniM0613 <wwh.rj.92@gmail.com>
2024-05-27 13:21:01 -0600

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Sidebar from "./components/Sidebar"
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      App
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar/>
+      </div>
     </div>
   )
 }
+export default App
